3b2e874d3bb533a351cb438241ecadb2316d3c7d
codsebas <arosalesr13@miumg.edu.gt>
2025-04-20 19:06:14 -0600

    Arregla TODA la base de datos

--- tablas principales.sql
+++ tablas principales.sql
@@ -4,62 +4,52 @@ SET search_path TO assenta_db;
 
 CREATE TABLE empleado (
     id_empleado SERIAL PRIMARY KEY,
-    dpi_empleado VARCHAR(13),
-    otro_doc_id VARCHAR(20), -- Si la persona no tuviera DPI y tuviera que dar pasaporte, yo que se
+    dpi_empleado VARCHAR(13) NOT NULL,
     sexo_empleado VARCHAR(50) NOT NULL,
     estado_civil VARCHAR(1) NOT NULL,  -- C=Casado, S=Soltero
     nombre1_empleado VARCHAR(50) NOT NULL,
-    nombre2_empleado VARCHAR(50) NOT NULL,
-    nombre3_empleado VARCHAR(50) NOT NULL,
+    nombre2_empleado VARCHAR(50),
+    nombre3_empleado VARCHAR(50),
     apellido1_empleado VARCHAR(50) NOT NULL,
-    apellido2_empleado VARCHAR(50) NOT NULL,
-    apellidocasada_empleado VARCHAR(50) NOT NULL,
+    apellido2_empleado VARCHAR(50),
+    apellidocasada_empleado VARCHAR(50),
     fec_nacimiento DATE NOT NULL,  -- Formato ISO
     edad_empleado INT NOT NULL,
     puesto_id INT NOT NULL,
     email_empleado VARCHAR(100),
+    telefono1_empleado VARCHAR(15) NOT NULL,
+    telefono2_empleado VARCHAR(15),
+    horario_entrada TIME NOT NULL,
+    horario_salida TIME NOT NULL,
     jefe_inmediato_id INT,  -- Nueva columna para el jefe
     CONSTRAINT fk_empleado_puesto FOREIGN KEY (puesto_id) REFERENCES puesto(id_puesto),
     CONSTRAINT fk_jefe_inmediato FOREIGN KEY (jefe_inmediato_id) REFERENCES empleado(id_empleado)  -- Relación recursiva
 );
 
-
-CREATE TABLE puesto(
-    id_puesto serial primary key,
-    nombre_puesto varchar(100) not null unique,
-    desc_puesto varchar(200) not null
+CREATE TABLE puesto( --SOLO PUEDE TENER UN PUESTO
+    id_puesto SERIAL PRIMARY KEY ,
+    nombre_puesto VARCHAR(50) NOT NULL UNIQUE,
+    desc_puesto VARCHAR(200) NOT NULL
 );
 
-CREATE TABLE telefono_empleado(
-    id_telefono_empleado serial primary key,
-    empleado_id int not null,
-    telefono_empleado varchar(15) not null,
-    constraint fk_empleado_telefono foreign key (empleado_id) references empleado(id_empleado)
+CREATE TABLE direccion_empleado( --SOLO UNA DIRECCIÓN DE CONTACTO
+    empleado_id INT NOT NULL PRIMARY KEY ,
+    departamento VARCHAR(50) NOT NULL,
+    municipio VARCHAR(50) NOT NULL ,
+    aldea VARCHAR(50),
+    direccion VARCHAR(100),
+    CONSTRAINT fk_empleado_direcccion FOREIGN KEY (empleado_id) REFERENCES empleado(id_empleado)
 );
 
-CREATE TABLE direccion_empleado(
-    id_direccion_empleado serial primary key,
-    empleado_id int not null,
-    departamento varchar(50) not null,
-    municipio varchar(50) not null,
-    aldea varchar(50) not null,
-    direccion varchar(100),
-    constraint fk_empleado_direcccion foreign key (empleado_id) references empleado(id_empleado)
+CREATE TABLE usuarios(
+    id_usuario SERIAL PRIMARY KEY,
+    usuario VARCHAR(15),
+    password VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE horario_empleado(
-    id_horario_empleado serial primary key,
-    empleado_id int not null unique,
-    horario_entrada time not null,
-    horario_salida time not null,
-    constraint fk_empleado_horario foreign key (empleado_id) references empleado(id_empleado)
-);
-
-CREATE TABLE jornada_empleado(
-    id_jornada_empleado serial primary key,
-    empleado_id int not null unique,
-    tipo_jornada varchar(15) not null, --Matutina, Vespertina,
-    dia_inicio_jornada varchar(15) not null, --Lunes, Martes, Miercoles, Jueves, Viernes, Sábado, Domingo
-    dia_fin_jornada varchar(15) not null,
-    constraint fk_empleado_jornada foreign key (empleado_id) references empleado(id_empleado)
+CREATE TABLE huella(
+    id_relacion_huellas SERIAL PRIMARY KEY,
+    empleado_id INT NOT NULL,
+    huella BYTEA not null,
+    CONSTRAINT fk_huella_empleado FOREIGN KEY (empleado_id) REFERENCES empleado(id_empleado)
 );
